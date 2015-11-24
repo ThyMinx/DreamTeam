77b58f0543eae2f4a29eac3f208ba30696a0822a
ThyMinx <james.cairns@outlook.com>
2015-11-24 11:14:29 +0000

    James - more back end added
    Added:
    - Bullet kills enemy and adds to score
    - When enemy dies explosion appears
    - Bullet can kill player
    - Player can explode
    
    TODO:
    - Explosion effect/animation
    - Enemy shooting script (shoot down only)
    - Enemy move script (it will move down slowly and side to side)
    - Enemy spawning

---
+++ IAN/Assets/DestroyByTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float waitTime;
+
+    void Update()
+    {
+        waitTime = waitTime - Time.deltaTime;
+
+        if (waitTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ IAN/Assets/Game/Scripts/DestroyByContact.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public int scoreValue;
+    public GameObject enemyExplosion;
+    public GameObject playerExplosion;
+    private GameManager gM;
+
+    void Start()
+    {
+        GameObject gMObj = GameObject.FindGameObjectWithTag("GameController");
+        gM = gMObj.GetComponent<GameManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (enemyExplosion != null && other.tag == "Enemy")
+        {
+            Debug.Log("Hit Enemy");
+            Debug.Log("BOOM!");
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
+            gM.score += scoreValue;
+        }
+
+        if (playerExplosion != null && other.tag == "Player")
+        {
+            Debug.Log("Hit Player");
+            Debug.Log("KABOOM!");
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
