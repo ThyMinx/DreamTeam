b32d682a9f0aade1765c165046974c59c985e116
ThyMinx <james.cairns@outlook.com>
2015-11-24 14:07:04 +0000

    James - Enemy Spawning

---
+++ IAN/Assets/Game/Scripts/Spawning.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawning : MonoBehaviour {
+
+    //Array of game objects called enemy
+    public GameObject[] enemy;
+
+    //Time between each spawn
+    public float spawnTime = 3f;
+
+    //Position of spawning defined by an object with a transform
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        //Repeat specified function in seconds after every spawnTime
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //Creates a random variable which is a random game object in the array enemy
+        int enemyIndex = Random.Range(0, enemy.Length);
+
+        //Creates an enemy based on which ever random one was chosen. It's created at the transform spawnPoint's position and rotation
+        Instantiate(enemy[enemyIndex], spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
